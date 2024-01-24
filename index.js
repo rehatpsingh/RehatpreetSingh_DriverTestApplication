06828e61f115bee769d515dd7011eadc5c93a510
rehatpsingh <rehatpsingh@gmail.com>
2024-01-24 10:24:05 -0500

    Updated the application using template engine ejs

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 //Modules
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
 
 //Creating Application
 const app = express();
 
 //Mid Ware
 app.use(express.static('public'))
+app.set('view engine', 'ejs')
 
-// get routing
-
-//1. Dashboard route
-app.get('/',(req, res) => {
-  res.sendFile(path.resolve(__dirname,'view/dashboard.html'));
+//get routing using render
+//1. Index route
+app.get('/', (req,res)=> {
+  res.render('dashboard');
 })
 
-//2. Login route
-app.get('/login',(req, res) => {
-  res.sendFile(path.resolve(__dirname,'view/login.html'));
+//2. G route
+app.get('/G', (req,res)=> {
+  res.render('G');
 })
 
-//3. G route
-app.get('/g',(req, res) => {
-  res.sendFile(path.resolve(__dirname,'view/G.html'));
+//3. G2 route
+app.get('/G2', (req,res)=> {
+  res.render('G2');
 })
 
-//4. G2 route
-app.get('/g2',(req, res) => {
-  res.sendFile(path.resolve(__dirname,'view/G2.html'));
+//4. Login route
+app.get('/Login',(req, res) => {
+  res.render('Login');
 })
 
 app.listen(4000, () => {console.log("Application link : http://localhost:4000/")});
